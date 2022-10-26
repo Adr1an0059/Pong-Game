a77a490186b65b598034dfe261ec62f732f3696b
Eddy <eacortezpatino@gmail.com>
2022-10-26 16:21:45 -0400

    Add score, winner, (now is playable)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Import library
+# Import libraries
 import pygame
 import random as rd
 
@@ -30,8 +30,8 @@ ball_x = 400
 ball_y = 300
 ball_radius = 20
 
-ball_speed_x = 0.4
-ball_speed_y = 0.4
+ball_speed_x = 0.5
+ball_speed_y = 0.5
 
 # window size
 screen_width = 800
@@ -45,6 +45,39 @@ screen = pygame.display.set_mode(size)
 
 # Title
 pygame.display.set_caption("Pong Game")
+
+# Score variables
+player_1_score = 0
+player_2_score = 0
+
+# Score font
+score_font = pygame.font.Font("Font.ttf", 32)
+
+# Win font
+win_font = pygame.font.Font("Font.ttf", 64)
+
+
+# Score position in the screen (player 1)
+player_1_score_x = 10
+player_1_score_y = 10
+
+# Score position in the screen (player 2)
+player_2_score_x = screen_width - 165
+player_2_score_y = 10
+
+# Win text position
+win_x = 260
+win_y = 220
+
+# Player 1 score function
+def show_score_1(x, y):
+    score_1 = score_font.render("Player one: " + str (player_1_score), True, (0, 0, 0))
+    screen.blit(score_1, (x, y))
+
+def show_score_2(x, y):
+    score_2 = score_font.render("Player two: " + str (player_2_score), True, (0, 0, 0))
+    screen.blit(score_2, (x, y))
+
 
 # Icon
 icon = pygame.image.load("icon.png")
@@ -64,17 +97,17 @@ while running:
             
             # Player 1
             if event.key == pygame.K_w:
-                player_1_y_speed = -0.7
+                player_1_y_speed = -0.9
 
             if event.key == pygame.K_s:
-                player_1_y_speed = 0.7
+                player_1_y_speed = 0.9
             
             # Player 2
             if event.key == pygame.K_UP:
-                player_2_y_speed = -0.7
+                player_2_y_speed = -0.9
 
             if event.key == pygame.K_DOWN:
-                player_2_y_speed = 0.7
+                player_2_y_speed = 0.9
 
         # Checks for KEYUP event
         if event.type == pygame.KEYUP:
@@ -99,11 +132,17 @@ while running:
     
     # Ball boundaries (right or left) and score update
     if ball_x > screen_width:
+
+        player_1_score += 1
+
         ball_x = screen_width/2
         ball_y = screen_height/2
         ball_speed_x *= rd.choice([-1, 1])
 
     elif ball_x < 0:
+
+        player_2_score += 1
+
         ball_x = screen_width/2
         ball_y = screen_height/2
         ball_speed_x *= rd.choice([-1, 1])
@@ -126,7 +165,34 @@ while running:
 
     # Fill the screen with color
     screen.fill(background_color)
+
+    # Draw the center line
+    pygame.draw.line(screen, line_color,(screen_width/2,0),(screen_width/2, screen_height))
+
+    # Draw the ball
+    ball = pygame.draw.circle(screen, ball_color,(ball_x, ball_y), ball_radius)
+
+    # Player win
+    if player_1_score >= 10:
+        player_1_y_speed = 0
+        player_2_y_speed = 0
+        ball_y = 2000
+        ball_speed_x = 0
+        ball_speed_y = 0
+        player_y = 0
+        win_text = win_font.render("PLAYER 1 WIN", True, (17, 7, 131))
+        screen.blit(win_text, (win_x, win_y))
     
+    elif player_2_score >= 10:
+        player_1_y_speed = 0
+        player_2_y_speed = 0
+        ball_y = 2000
+        ball_speed_x = 0
+        ball_speed_y = 0
+        player_y = 0
+        win_text = win_font.render("PLAYER 2 WIN", True, (17, 7, 131))
+        screen.blit(win_text, (win_x, win_y))
+
     # Drawing area
 
     # Define the player 1 - left: rectangle
@@ -135,11 +201,15 @@ while running:
     # Define the player 2 - right: rectangle
     player_2 = pygame.draw.rect(screen, player_color, (player_2_x, player_2_y, players_width, players_height))
 
-    # Draw the center line
-    pygame.draw.line(screen, line_color,(screen_width/2,0),(screen_width/2, screen_height))
+    # Colitions
+    if ball.colliderect(player_1) or ball.colliderect(player_2):
+        ball_speed_x *= -1
 
-    # Draw the ball
-    ball = pygame.draw.circle(screen, ball_color,(ball_x, ball_y), ball_radius)
+    # Call the show score_1 function
+    show_score_1(player_1_score_x, player_1_score_y)
+
+    # Call the show score_2 function
+    show_score_2(player_2_score_x, player_2_score_y)
 
     # Update the window
     pygame.display.flip()
